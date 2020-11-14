0cf6ac79ad7b4d6bfd683fe467061cc00609cb04
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2020-11-14 10:04:56 -0600

    added a function to save input to local storage.added event listener to the save buttons.

--- Develop/script.js
+++ Develop/script.js
@@ -7,15 +7,45 @@ $("#currentDay").text(now.format('MMMM Do YYYY'));
 for (var i = 9; i < 18; i++) {
     var timeBlock = $("<div class='time-block'></div>")
     var timeSpan = $("<span class='time-span'>" + i + ":00</span>")
-    var input = $('<input class="description"></input>')
+    var input = $('<input class="description" data-hour="'+i+'"></input>')
+
+    //use local storage to link the i and previous inputs to auto fill the data.
+
+    var oldInput = localStorage.getItem(i);
+    input.val(oldInput);
     var button = $("<button class='saveBtn'>Save</button>")
+
     timeBlock.append(timeSpan, input, button);
     $(".container").append(timeBlock);
 
 }
-
+//uses input and pulls it back when refreshed.
+function saveBtnHandler (event) {
+    var userInput = $(event.target).siblings("input").val();
+    var Time = $(event.target).siblings("input").data("hour");
+    localStorage.setItem( Time , userInput);
+}
 
 //add event listener to all the save buttons that grabs the input value for the corresponding time-block.
+var saveButtons = document.getElementsByClassName('saveBtn');
+for(var i = 0; i < saveButtons.length; i++) {
+    saveButtons[i].addEventListener("click", saveBtnHandler);
+}
 
 
-//use local storage to link the i and previous inputs to auto fill the data.
+
+function time () {
+    var time = something
+    if (time == now.hours) {
+        //code goes in here to add css to color code the page to present
+    
+    }  else if (time < now.hours) {
+        //code goes in here to add css to color code the page to past
+    
+    } else {
+        //code goes in here to add css to color code the page to future
+    
+    }
+
+
+}
